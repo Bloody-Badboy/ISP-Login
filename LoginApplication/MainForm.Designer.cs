9470a7c6b3993d40f6088e37b5ad8f80c8a08842
BloodyBadboyCreation <BloodyBadboyCreation@GHOST007>
2017-07-17 07:52:12 +0530

    Add project files.

---
+++ LoginApplication/MainForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace LoginApplication
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.username_label = new System.Windows.Forms.Label();
+            this.password_label = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.showPassCheckBox = new System.Windows.Forms.CheckBox();
+            this.log_inout_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(15, 28);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(260, 20);
+            this.userNameTextBox.TabIndex = 0;
+            // 
+            // username_label
+            // 
+            this.username_label.AutoSize = true;
+            this.username_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username_label.Location = new System.Drawing.Point(12, 9);
+            this.username_label.Name = "username_label";
+            this.username_label.Size = new System.Drawing.Size(71, 16);
+            this.username_label.TabIndex = 1;
+            this.username_label.Text = "Username";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password_label.Location = new System.Drawing.Point(12, 60);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(68, 16);
+            this.password_label.TabIndex = 2;
+            this.password_label.Text = "Password";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(15, 79);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(257, 20);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // showPassCheckBox
+            // 
+            this.showPassCheckBox.AutoSize = true;
+            this.showPassCheckBox.Location = new System.Drawing.Point(15, 115);
+            this.showPassCheckBox.Name = "showPassCheckBox";
+            this.showPassCheckBox.Size = new System.Drawing.Size(101, 17);
+            this.showPassCheckBox.TabIndex = 4;
+            this.showPassCheckBox.Text = "Show password";
+            this.showPassCheckBox.UseVisualStyleBackColor = true;
+            this.showPassCheckBox.CheckedChanged += new System.EventHandler(this.showPassCheckBox_CheckedChanged);
+            // 
+            // log_inout_button
+            // 
+            this.log_inout_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.log_inout_button.Location = new System.Drawing.Point(18, 138);
+            this.log_inout_button.Name = "log_inout_button";
+            this.log_inout_button.Size = new System.Drawing.Size(257, 31);
+            this.log_inout_button.TabIndex = 5;
+            this.log_inout_button.Text = "Log In";
+            this.log_inout_button.UseVisualStyleBackColor = true;
+            this.log_inout_button.Click += new System.EventHandler(this.login_button_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 180);
+            this.Controls.Add(this.log_inout_button);
+            this.Controls.Add(this.showPassCheckBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.username_label);
+            this.Controls.Add(this.userNameTextBox);
+            this.Name = "MainForm";
+            this.Text = "ISP Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Label username_label;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.CheckBox showPassCheckBox;
+        private System.Windows.Forms.Button log_inout_button;
+    }
+}
+
---
+++ LoginApplication/MainForm.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.String;
+using static Microsoft.Win32.Registry;
+
+namespace LoginApplication
+{
+    public partial class MainForm : Form
+    {
+        private string _response;
+        private string _userName;
+        private string _password;
+        private bool _logedIn = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void login_button_Click(object sender, EventArgs e)
+        {
+            _userName = userNameTextBox.Text;
+            _password = passwordTextBox.Text;
+
+            if (_userName.Trim().Length == 0)
+            {
+                MessageBox.Show("Please enter user name", "Input Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                userNameTextBox.Focus();
+                return;
+            }
+
+            if (_password.Trim().Length == 0)
+            {
+                MessageBox.Show("Please enter password", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                passwordTextBox.Focus();
+                return;
+            }
+
+            var registryKey = CurrentUser.OpenSubKey(@"SOFTWARE\ISP LoginApplication", true);
+            if (registryKey != null)
+            {
+                registryKey.SetValue("username", _userName);
+                registryKey.SetValue("password", _password);
+                registryKey.Close();
+            }
+
+            var loginWorker = new BackgroundWorker();
+            loginWorker.DoWork += Login_WorkerOnDoWork;
+            loginWorker.RunWorkerCompleted += Login_WorkerOnRunWorkerCompleted;
+
+            log_inout_button.Enabled = false;
+            userNameTextBox.Enabled = false;
+            passwordTextBox.Enabled = false;
+            showPassCheckBox.Enabled = false;
+            username_label.Enabled = false;
+            password_label.Enabled = false;
+
+
+            loginWorker.RunWorkerAsync();
+        }
+
+
+        private void Login_WorkerOnDoWork(object sender, DoWorkEventArgs e)
+        {
+            _response = null;
+            var postParams = $"login=1&user={_userName}&pass={_password}&version=0.2.23.0";
+            if (_logedIn)
+            {
+                postParams = $"logout=1&user={_userName}&pass={_password}&version=0.2.23.0";
+            }
+            var postBytes = Encoding.UTF8.GetBytes(postParams);
+
+            var webRequest = WebRequest.Create("http://10.254.254.53/0/wl/");
+            webRequest.Method = "POST";
+            webRequest.Timeout = 10000;
+            webRequest.ContentLength = postBytes.Length;
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+
+            try
+            {
+                webRequest.GetRequestStream().Write(postBytes, 0, postBytes.Length);
+
+                var httpWebResponse = (HttpWebResponse) webRequest.GetResponse();
+
+
+                if (httpWebResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    using (var responseStream = httpWebResponse.GetResponseStream())
+                    {
+                        if (responseStream != null)
+                        {
+                            _response = new StreamReader(responseStream).ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (WebException exception)
+            {
+            }
+        }
+
+        private void Login_WorkerOnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (_response == null)
+            {
+                MessageBox.Show("Error!!");
+                return;
+            }
+            var stringReader = new StringReader(_response);
+            var line = stringReader.ReadLine();
+            if (line != null && line.Equals("OK", StringComparison.OrdinalIgnoreCase))
+            {
+                if (!_logedIn)
+                {
+                    MessageBox.Show("Login Successfully!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    _logedIn = true;
+                    log_inout_button.Text = "Logout";
+                    log_inout_button.Enabled = true;
+                    userNameTextBox.Enabled = false;
+                    passwordTextBox.Enabled = false;
+                    showPassCheckBox.Enabled = false;
+                    username_label.Enabled = false;
+                    password_label.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("Logout Successfully!!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    log_inout_button.Text = "Log In";
+                    _logedIn = false;
+                }
+            }
+
+
+            log_inout_button.Enabled = true;
+            userNameTextBox.Enabled = true;
+            passwordTextBox.Enabled = true;
+            showPassCheckBox.Enabled = true;
+            username_label.Enabled = true;
+            password_label.Enabled = true;
+        }
+
+        private void LoginCheck_WorkerOnDoWork(object sender, DoWorkEventArgs e)
+        {
+            _response = null;
+
+            var webRequest = WebRequest.Create("http://10.254.254.53/0/wl/cklogin");
+            webRequest.Method = "GET";
+            webRequest.Timeout = 10000;
+
+            try
+            {
+                var httpWebResponse = (HttpWebResponse) webRequest.GetResponse();
+
+                if (httpWebResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    using (var responseStream = httpWebResponse.GetResponseStream())
+                    {
+                        if (responseStream != null)
+                        {
+                            _response = new StreamReader(responseStream).ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (WebException exception)
+            {
+            }
+
+            if (_response != null)
+            {
+                var stringReader = new StringReader(_response);
+                var line = stringReader.ReadLine();
+                if (line != null && line.Equals("YES", StringComparison.OrdinalIgnoreCase))
+                {
+                    _logedIn = true;
+                }
+            }
+        }
+
+        private void LoginCheck_WorkerOnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (_logedIn)
+            {
+                log_inout_button.Enabled = true;
+                log_inout_button.Text = "Logout";
+            }
+            else
+            {
+                log_inout_button.Enabled = true;
+                userNameTextBox.Enabled = true;
+                passwordTextBox.Enabled = true;
+                showPassCheckBox.Enabled = true;
+                username_label.Enabled = true;
+                password_label.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var registryKey =
+                CurrentUser.OpenSubKey(@"SOFTWARE\ISP LoginApplication");
+            if (registryKey == null)
+            {
+                registryKey = CurrentUser.CreateSubKey(@"SOFTWARE\ISP LoginApplication");
+            }
+            userNameTextBox.Text = (string) registryKey.GetValue("username");
+            passwordTextBox.Text = (string) registryKey.GetValue("password");
+            registryKey.Close();
+
+            var loginCheck_Worker = new BackgroundWorker();
+            loginCheck_Worker.DoWork += LoginCheck_WorkerOnDoWork;
+            loginCheck_Worker.RunWorkerCompleted += LoginCheck_WorkerOnRunWorkerCompleted;
+
+            log_inout_button.Enabled = false;
+            userNameTextBox.Enabled = false;
+            passwordTextBox.Enabled = false;
+            showPassCheckBox.Enabled = false;
+            username_label.Enabled = false;
+            password_label.Enabled = false;
+
+            loginCheck_Worker.RunWorkerAsync();
+        }
+
+
+        private void showPassCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            passwordTextBox.UseSystemPasswordChar = showPassCheckBox.CheckState != CheckState.Checked;
+        }
+    }
+}
